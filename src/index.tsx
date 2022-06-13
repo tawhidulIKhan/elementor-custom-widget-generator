0a7f741262698c595d29667455e61910d6362644
tawhidulIKhan <tawhid.developer@gmail.com>
2022-06-13 20:47:08 +0600

    style: import antd css

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { store } from './store';
+import 'antd/dist/antd.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
