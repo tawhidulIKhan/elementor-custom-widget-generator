41dbecb6d8b40e8ef826cd3eff78c6226de23313
tawhidulIKhan <tawhid.developer@gmail.com>
2022-06-13 20:44:59 +0600

    feat: add widgetGeneratorPage, Form, Code

---
+++ src/components/widgetCode/WidgetCode.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function WidgetCode() {
+  return <div>WidgetCode</div>;
+}
--- src/pages/WidgetGenerator.tsx
+++ src/pages/WidgetGenerator.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import WidgetForm from '../components/widgetForm/WidgetForm';
+import './WidgetGenerator.scss';
 
 function WidgetGenerator() {
-  return <div>Widget </div>;
+  return (
+    <div className="wg">
+      <div className="wg__content">
+        <div className="wg__content__left">
+          <WidgetForm />
+        </div>
+        <div className="wg__content__right" />
+      </div>
+    </div>
+  );
 }
 
 export default WidgetGenerator;
